7624e2efdde414468c5a406cccfadb52c4e3bd6f
mrbaggiebug <mani.ehsanisohi@gmail.com>
2023-07-19 22:19:57 +0430

    hol up

--- src/stream/streamer.ts
+++ src/stream/streamer.ts
@@ -4,22 +4,18 @@ import nms from "node-media-server"
 import cp from "child_process"
 import "dotenv/config"
 
-let dick = () => {
-    cp.spawn("ffmpeg", [
-        '-i',
-        '../baked-video/0done.mp4',
-        '-vf',
-        'scale=1280:960',
-        '-vcodec',
-        'libx264',
-        '-profile:v',
-        'baseline',
-        '-pix_fmt',
-        'yuv420p',
-        '-f',
-        'flv',
-        'rtmp://muc01.contribute.live-video.net/app/'
-    ])
-}
-
-dick()
+cp.spawn("ffmpeg", [
+    '-i',
+    '../baked-video/0done.mp4',
+    '-vf',
+    'scale=1280:960',
+    '-vcodec',
+    'libx264',
+    '-profile:v',
+    'baseline',
+    '-pix_fmt',
+    'yuv420p',
+    '-f',
+    'flv',
+    'rtmp://muc01.contribute.live-video.net/app/'
+])
